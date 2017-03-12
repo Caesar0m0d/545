a315ed3e9ce1bb902f9ac1dda42f26b0c69b48dc
Caesar0m0d <Caesar0m0d@gmail.com>
2017-03-13 06:58:22 +0900

    545E.c

---
+++ 545E.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+long long n,m,ui,vi,wi,u,l;
+long long * arr[30000];
+		for(l=0;l<30000;l++)arr[l] = (long long *)malloc(sizeof(long long)*30000);
+/*
+scanf("%lld %lld",&n,&m);
+		for(l=0;l<m;l++){
+scanf("%lld %lld %lld",&ui,&vi,&wi);
+arr[ui][vi] = wi;
+				}
+scanf("%lld",&u);
+*/
+//test
+arr[3][3] = 1;
+printf("%lld",arr[3][3]);
+//test
+	  }
